dee5bc10aafc364d852b0aa4650516965b27bac7
kevinjapan <61291412+kevinjapan@users.noreply.github.com>
2024-10-10 11:40:05 +0100

    add loaders for album views

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App.tsx'
+import HomeView from './views/HomeView.tsx'
 import AboutView from './views/AboutView.tsx'
-import AlbumsView from './views/AlbumsView.tsx'
+import AlbumsListView from './views/AlbumsListView.tsx'
+import AlbumView from './views/AlbumView.tsx'
 import ErrorView from './views/ErrorView.tsx'
+import { get_albums_list } from './queries/albums/get_albums_list.ts'
+import { get_album } from './queries/albums/get_album.ts'
 import './index.css'
+
+
 
 // to do : convention for hosting this router in a separate file / folder - where? name?
 const router = createBrowserRouter([
@@ -15,12 +21,28 @@ const router = createBrowserRouter([
       errorElement:<ErrorView />,
       children:[
          {
-         path: "about",
-         element: <AboutView />,
+            path: "home",
+            element: <HomeView />,
+         },
+         {
+            path: "about",
+            element: <AboutView />,
          },
          {
             path:'albums',
-            element:<AlbumsView/>,
+            element:<AlbumsListView/>,
+            loader:() => {
+               return get_albums_list()               
+            }
+         },
+         {
+            path:'albums/:slug',
+            element:<AlbumView/>,
+            loader:({params}) => {               
+               return get_album(params.slug as string)
+            }
+            
+               // return fetch('/api/users' + params.slug)
          },
       ]
    },
