0685a4b38f0321fc68538c2c1b15f4280dea5586
kevinjapan <61291412+kevinjapan@users.noreply.github.com>
2024-10-11 11:16:06 +0100

    remove router to separate file

--- src/main.tsx
+++ src/main.tsx
@@ -1,53 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import App from './App.tsx'
-import HomeView from './views/HomeView.tsx'
-import AboutView from './views/AboutView.tsx'
-import AlbumsListView from './views/AlbumsListView.tsx'
-import AlbumView from './views/AlbumView.tsx'
-import ErrorView from './views/ErrorView.tsx'
-import { get_albums_list } from './queries/albums/get_albums_list.ts'
-import { get_album } from './queries/albums/get_album.ts'
+import { RouterProvider } from 'react-router-dom'
 import './index.css'
+import get_router  from './router/index'
 
 
-
-// to do : convention for hosting this router in a separate file / folder - where? name?
-const router = createBrowserRouter([
-   {
-      path:'/',
-      element:<App/>,
-      errorElement:<ErrorView />,
-      children:[
-         {
-            path: "home",
-            element: <HomeView />,
-         },
-         {
-            path: "about",
-            element: <AboutView />,
-         },
-         {
-            path:'albums',
-            element:<AlbumsListView/>,
-            loader:() => {
-               return get_albums_list()               
-            }
-         },
-         {
-            path:'albums/:slug',
-            element:<AlbumView/>,
-            loader:({params}) => {               
-               return get_album(params.slug as string)
-            }
-            
-               // return fetch('/api/users' + params.slug)
-         },
-      ]
-   },
-])
-
+const router = get_router()
 
 createRoot(document.getElementById('root')!).render(
    <StrictMode>
