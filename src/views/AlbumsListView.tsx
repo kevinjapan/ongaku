86f5f81067950450ee61335976cb218ad923516b
kevinjapan <61291412+kevinjapan@users.noreply.github.com>
2024-10-14 11:35:35 +0100

    use AlbumTeaser

--- src/views/AlbumsListView.tsx
+++ src/views/AlbumsListView.tsx
@@ -1,17 +1,18 @@
-import { useLoaderData, Link } from 'react-router-dom'
+import { useLoaderData } from 'react-router-dom'
 import HeroBanner from '../components/HeroBanner/HeroBanner'
+import AlbumTeaser from '../components/Albums/AlbumTeaser/AlbumTeaser'
 
 
 export default function AlbumsListView() {
 
+   // Type Assertion (Type Casting) - ' as ' or <AlbumsList>useLoaderData()
+   // but we can't use <...? in react tsx files
    const { albums } = useLoaderData() as AlbumsList // This hook provides the value returned from your route loader.
 
 
    // to do : fade_in etc won't work since it only runs on index.html load (as w/ static edk site)
-   //         we need to run/initiate on every component load
-
-
-   // to do : effect fade_in on cover_block on component mounting?
+   // we need to run/initiate on every component load
+   // effect fade_in on cover_block on component mounting?
 
    return (
       <>
@@ -21,25 +22,23 @@ export default function AlbumsListView() {
             featureImg="/assets/imgs/all-sorts-of-questions.jpg"
          />
 
-         <h3>AlbumsListView</h3>
+         <h3 className="ml_2">Albums</h3>
+
          
-         {albums.length ? (
-               <ul>
-               {albums.map((album: Album) => (
-                  <li key={album.id}>
-                     <Link to={`${album.slug}`}>                   
-                        <>
-                           {album.title} 
-                        </>
-                     </Link>
-                  </li>
-               ))}
-               </ul>
-            ) : (
-               <p>
-               <i>No albums</i>
-               </p>
-            )}
+         <section className="feature_tiles">
+            <ul>
+               {albums.length ? 
+                  albums.map((album: Album) => (                 
+                     <AlbumTeaser
+                        key={album.id}  
+                        album={album} 
+                     />
+                  ))
+                  : (
+                     <p><i>No albums</i></p>
+                  )}
+            </ul>
+         </section>
 
       </>
    )
