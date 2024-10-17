a8aab95cfd9e8d74407134c2505a10e9cbc62077
kevinjapan <61291412+kevinjapan@users.noreply.github.com>
2024-10-17 13:03:31 +0100

    fix AppNav responsive (on-going)

--- src/components/App/AppNav/AppNav.tsx
+++ src/components/App/AppNav/AppNav.tsx
@@ -1,23 +1,53 @@
 import { useState } from 'react'
 import { Link } from 'react-router-dom'
-// import { useRef } from 'react'
+import { is_sm_screen } from '../../../utilities/screen/screen'
 
 
 // AppNav
 // our nav was built around keeping dropdown open on hover, which worked fine with new pages reloading the nav; 
 // in this SPA, however, the hover works against us, keeping the dropdown open when entering a new view
-// workaround - we useState w/ 'reset' to reset the nav on new views opening
-// we lose the fade/slide-up animation but do get clean views
+// workaround - we 'reset' the nav on new views opening. We lose the fade/slide-up animation but get clean views
 // future : still slight awkward on clicking on top level nav - re-opens dropdown abruptly
 
+// to do : bring in hiding nav on scroll down / show nav on scroll up (see orig. EDK) (transparent over hero_block only)
+// to do : bug : click on logo, opens dropdown immediately.
+// to do : bug : on sm, dropdown loses bg color before scrolling up to hidden position
+// to do : improve transition on opening new screen - scroll up nav (?) / fade in new hero_banner cover block img
 
 export default function AppNav() {
 
+   // reset nav on opening new view (we close extended dropdown)
    const [reset, setReset] = useState(true)
+
+   // track extended and toggle extended_nav_dropdown
+   const [extended, setExtended] = useState(false)
+
+
+   // methods
 
    const reset_nav = () => {
       setReset(false)
       setTimeout(() => setReset(true),500)
+      if(is_sm_screen()) toggle()
+   }
+
+   const toggle = () => {
+
+      const dropdown = document.querySelector('nav ul.nav_list')
+      const nav_toggle = document.querySelector('.nav_toggle')
+
+      if(dropdown && nav_toggle) {
+         // classList.toggle was not working, so we use our own 'extended' state to track
+         if(extended) {
+            dropdown.classList.remove('extended_nav_dropdown')
+            nav_toggle.classList.remove('selected_toggle')
+         }
+         else {
+            dropdown.classList.add('extended_nav_dropdown')
+            nav_toggle.classList.add('selected_toggle')
+         }
+         setExtended(!extended)
+      }
    }
 
    // to do : expand - get dropdown menu items from these objects:
@@ -53,29 +83,32 @@ export default function AppNav() {
             }
          ]
       },
-      {label:'About',route:'about'},
+      {label:'About',route:'about',
+         children:[
+            {
+               id:1,
+               label:'beneath the waves',
+               route:'/beneath-the-waves'
+            },]},
    ]
 
    return (
       <nav id="nav" className="nav">
 
          <div className="logo_block">
-            <Link to="/">edk</Link>
+            <Link to="/" >edk</Link>
          </div>
 
-         { /* to do : separate component */}
-         <div className="nav_toggle_wrap">
-            <div className="nav_toggle">
-               <div className="toggle_bar"></div>
-               <div className="toggle_bar"></div>
-               <div className="toggle_bar"></div>
-            </div>
-         </div>  
+         <div className="nav_toggle" onClick={toggle}>
+            <div className="toggle_bar"></div>
+            <div className="toggle_bar"></div>
+            <div className="toggle_bar"></div>
+         </div>
 
-         <ul className="nav_list flex gap_2">
+         <ul className="nav_list gap_2">
             {nav_links.map(link => 
                <li key={link.label}>
-                  <Link to={`${link.route ? link.route : link.label}`} data-list="services" className="nav_list_label" onClick={reset_nav}>
+                  <Link to={`${link.route ? link.route : link.label}`} className="nav_list_label" onClick={reset_nav}>
                      {link.label}
                   </Link>
                   {reset ? 
