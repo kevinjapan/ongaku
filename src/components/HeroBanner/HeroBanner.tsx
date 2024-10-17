b87173d10ee3a2a52613e27d299d43c94d2bc4dc
kevinjapan <61291412+kevinjapan@users.noreply.github.com>
2024-10-17 11:53:56 +0100

    dim cover img

--- src/components/HeroBanner/HeroBanner.tsx
+++ src/components/HeroBanner/HeroBanner.tsx
@@ -5,10 +5,11 @@ export default function HeroBanner(props: HeroBannerProps) {
    // to do : place dimmer over img to make text clear
 
    return (
-      <section className="cover_block hero_block bg_navy ">
+      <section className="cover_block hero_block bg_navy dim_20">
          <img className="bg_img" src={props.featureImg} />
          <div className="overlay ">
             <h1>{props.overlayHeading}</h1>
+            {props.overlayTagline ? <h4>{props.overlayTagline}</h4> : null}
          </div>
       </section>
    )
