875e050cdbb25e05d86a573efce0d89ffc1c7169
kevinjapan <61291412+kevinjapan@users.noreply.github.com>
2024-10-22 12:50:56 +0100

    Create appNav.ts

---
+++ src/utilities/appNav/appNav.ts
@@ -0,0 +1,44 @@
+
+
+export function get_nav_links () {
+   return [
+      {
+         label:'Albums',
+         route:'albums',
+         children:[
+            {
+               id:1,
+               label:'beneath the waves',
+               route:'/beneath-the-waves'
+            },
+            {
+               id:2,
+               label:'river',
+               route:'/river'
+            },
+            {
+               id:3,
+               label:"life's a rhetorical question",
+               route:'/lifes-a-rhetorical-question'
+            },
+            {
+               id:4,
+               label:'rough not ready',
+               route:'/rough-not-ready'
+            },
+            {
+               id:5,
+               label:'the wee song sketchbook',
+               route:'/the-wee-song-sketchbook'
+            }
+         ]
+      },
+      {label:'About',route:'about',
+         children:[
+            {
+               id:1,
+               label:'beneath the waves',
+               route:'/beneath-the-waves'
+            },]},
+   ]
+}
