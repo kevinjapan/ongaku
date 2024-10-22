2a8b5a725305bcdd278a836c067a979713202299
kevinjapan <61291412+kevinjapan@users.noreply.github.com>
2024-10-22 12:51:53 +0100

    fix types etc on useData

--- src/utilities/endPoints/endPoints.ts
+++ src/utilities/endPoints/endPoints.ts
@@ -6,21 +6,24 @@
 // app_store.app_api is our flag - if emtpy, we are in 'web api' mode, else we are in 'static site' mode
 // url params : we assume order matches that expected by end-point and we append to url
    
-export function get_end_points() {
+export function get_end_points(url_params: Array<string>,query_params: QueryParams) {
 
+   console.log('to do : use query_params: ',query_params)
 
    // if(app_store.app_api === '') // to do : context equivalent for app_api path
    {
 
       // static site endpoints
-
       return {
-
          albums_list:{
             request_method:'GET',
             route:`/data/albums_list.json`
-         },
-         
+         },   
+         album:{
+            request_method:'GET',
+            route:`/data/albums/`,
+            route_url_params:`${url_params?.join('/')}.json`
+         },       
       }
 
    }
--- src/utilities/fetch/fetch.ts
+++ src/utilities/fetch/fetch.ts
@@ -1,10 +1,11 @@
 import status_code from '../statusCode/statusCode'
-import { is_empty_obj } from '../utilities/utilities'
+
 
 
 export async function use_fetch<T>(url: string, options: RequestInit): Promise<DataPackageContents<T>> {
 
    let data:object = {}
+   let outcome:string = ''
    let error:string = ''
 
    // remove any non-valid body (could break GET action)
@@ -20,14 +21,23 @@ export async function use_fetch<T>(url: string, options: RequestInit): Promise<D
          }
       }) 
       .then((json) => {
-         data = json
+         outcome = json.outcome
+         data = json.data
       })
       .catch((err) => {
          error = err
       })
 
+      // to do : there is a mismatch here - we are assigning 'outcome' below,
+      // but this is given in the json file
+      // secondly, we are packing the whole json file into 'data' - so it is not the type (eg Album) that we are 
+      // looking to pass to consumer
+      // decision - do we retain 'outcome' in json file - if so, we have to extract here..
+      // will have to extract from server response anyhow, so let's do same w/ json file
+
+
       return { 
-         outcome:is_empty_obj(data) ? 'fail' : 'success',
+         outcome:outcome as QueryOutcome,
          data: data as T,
          error: error
       }
