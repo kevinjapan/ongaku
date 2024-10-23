dc8ce8c88754290e45173f8e286a7f0d8ad0b651
kevinjapan <61291412+kevinjapan@users.noreply.github.com>
2024-10-23 13:21:03 +0100

    add AppContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,26 @@
 import { Suspense } from 'react'
 import { Outlet } from 'react-router-dom'
+import { AppContextProvider } from './AppContext'
 import './App.css'
 import AppNav from './components/App/AppNav/AppNav'
 import AudioPlayer from './components/AudioPlayer/AudioPlayer'
 import BubbleAnimator from './components/BubbleAnimator/BubbleAnimator'
 import AppFooter from './components/App/AppFooter/AppFooter'
 
-
+// interface AppContext {
+//    app_api:string,
+//    prev_feature_img:string
+// }
 
 export default function App() {
 
    
    return (
-      <>
+      <AppContextProvider>
+
          <AppNav/>
 
-         <main>
-            
+         <main>               
             <Suspense>
                <Outlet/>
             </Suspense>
@@ -27,6 +31,8 @@ export default function App() {
          <BubbleAnimator/>
 
          <AppFooter/>
-      </>
+
+      </AppContextProvider>
+      
    )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
-import './index.css'
 import get_router  from './router/index'
 
 
