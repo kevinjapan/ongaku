f2e446a316be5e8e6815368544726eea447b4ea6
kevinjapan <61291412+kevinjapan@users.noreply.github.com>
2024-10-23 14:24:55 +0100

    moved 'loader' funcs to components

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -3,6 +3,7 @@ import { createBrowserRouter } from 'react-router-dom'
 import App from '../App'
 
 
+// routes
 
 export default function get_router() {
 
@@ -27,41 +28,12 @@ export default function get_router() {
                element: <AboutView />,
             },
             {
-               // to do : loader is limiting me - not allowing eg context to be used,
-               //         whereas i expect i could easily access this in-site on AlbumsListView component -]
-               // so let's move this use_data call up to AlbumsListView and try to access AppContext.
                path:'albums',
                element:<AlbumsListView/>
-               // loader:async(api_path) => {
-
-               //    console.log('can i get api path from AlbumsListView here?',api_path)
-               //    let retrieved_data = null
-               //    try {
-               //       const { data, error, outcome } = await use_data('albums_list',[],{},null)
-               //       if(data) {
-               //          if(outcome === 'success') retrieved_data = data
-               //       }
-               //       else {
-               //          throw error
-               //       }
-               //    }
-               //    catch(error) {
-               //       return {
-               //          outcome: 'fail',
-               //          error:error as string,
-               //          data:{}
-               //       }
-               //    }
-               //    // note : we return an AlbumsList, w/ 'albums_list' key / array value pair
-               //    return {albums_list:retrieved_data?.albums_list}                   
-               // }
             },
             {
                path:'albums/:slug',
                element:<AlbumView/>
-               // loader:({params}) => {               
-               //    return get_album(params.slug as string)
-               // }
             },
          ]
       },
