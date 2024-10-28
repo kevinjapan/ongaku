575e537d0bcabb9ecee3d275bda8009b19b3d493
kevinjapan <61291412+kevinjapan@users.noreply.github.com>
2024-10-28 12:29:48 +0000

    use useData

--- src/views/AlbumView.tsx
+++ src/views/AlbumView.tsx
@@ -1,53 +1,58 @@
-import { useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom';
-import use_data from '../utilities/useData/useData'
+import { useRef, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import useData from '../hooks/useData/useData'
 import HeroBanner from '../components/HeroBanner/HeroBanner'
+import TrackCardsList from '../components/Tracks/TrackCardsList/TrackCardsList'
 
 
 
 // AlbumView
 
+// to do : we have duplicate same video link in json files:
+// i dont want to be here anymore / release your love / this picture
+
+// to do : backup track json files to github?
+
+
 export default function AlbumView() {
 
    const { slug } = useParams()
-   const [album, setAlbum] = useState<Album | null>(null)
-      
-   // load album
+   const slug_ref = useRef(slug)
+   
+   const { loading, data, error, updateUrl } = useData<UseDataReturn<Album>>(`/data/albums/${slug_ref.current}.json`)
+
+   // if 'slug' changes, we have to invoke useFetch to reload new url
    useEffect(() => {
+      slug_ref.current = slug
+      updateUrl(`/data/albums/${slug_ref.current}.json`)
+   },[slug,updateUrl])
 
-      async function fetchData() {
-         try {
-            const { data, error, outcome } = await use_data<Album>('album',[`${slug}`],{},null)
-            if(data) {
-               if(outcome === 'success') setAlbum(data)
-            }
-            else {
-               throw error
-            }
-         }
-         catch(error) {
-            console.log('error',error)
-         }
-      }
-      fetchData()
-   },[slug])
 
-   return (
-      <>
-         {album ?
-            <>
-               <HeroBanner 
-                  overlayHeading={album.title} 
-                  featureImg={album.feature_img}
-               />
-               <h4>{album.feature_img}</h4>
-               <h3>AlbumView</h3>
-               <p>{album.id}</p>
-               <p>{album.title}</p>
-               <p>{album.slug}</p>
-               <p>{album.created_at}</p>
-            </>
-            : null}
-      </>
-   )
+   if(loading) {
+      return <div>loading</div>
+   }
+
+   if(error) {
+      return <div>{error}</div>
+   }
+
+   // to do : data?.data?.xxx is awkward - tidy up interface
+
+   if(data) {
+
+      return (
+         <>
+            {data?.data ?
+               <>
+                  <HeroBanner 
+                     overlayHeading={data?.data?.title} 
+                     featureImg={data?.data?.feature_img}
+                  />
+                  
+                  <TrackCardsList tracks={data?.data?.tracks} />
+               </>
+               : null}
+         </>
+      )
+   }
 }
--- src/views/AlbumsListView.tsx
+++ src/views/AlbumsListView.tsx
@@ -1,4 +1,4 @@
-import useFetch from '../hooks/useFetch/useFetch'
+import useData from '../hooks/useData/useData'
 import HeroBanner from '../components/HeroBanner/HeroBanner'
 import AlbumTeaser from '../components/Albums/AlbumTeaser/AlbumTeaser'
 
@@ -8,18 +8,7 @@ import AlbumTeaser from '../components/Albums/AlbumTeaser/AlbumTeaser'
 
 export default function AlbumsListView() {
 
-   // to do : we need to make useFetch and useData into Custom Hooks
-   // 1 - get useFetch working from here
-   // 2 - get useData working as intermediate and then use from here w/ end-points
-   //     temp use url directly w/ useFetch() here - then replace w/ useData() and 'albums_list' once end_points are re-enabled:
-
-   const { 
-      loading, data, error 
-   } = useFetch<DataPackage<AlbumsList>>('/data/albums_list.json',{
-      headers: {accept: "application/json"},
-   },{
-      immediate: true, 
-   })
+   const { loading, data, error } = useData<UseDataReturn<AlbumsList>>('/data/albums_list.json')
 
    if(loading) {
       return <div>loading</div>
@@ -32,32 +21,33 @@ export default function AlbumsListView() {
    // to do : verify my types setup here permits/handles empty array - []
    // to do : review - just reduce to data/error (remove 'outcome' - superfluous?)
 
-   // to do : data?.data?.xxx is awkward - tidy up interface
+   // to do : data?.data?.xxx is awkward and a little unclear - 
+   //         works for now, but research further and re-visit types etc.
 
    if(data) {
-      console.log('GOT 2',data?.data?.albums_list)
 
-   return (
-      <>
-         <HeroBanner 
-            overlayHeading="albums" 
-            featureImg="/assets/imgs/all-sorts-of-questions.jpg"/>
+      console.log('IS',data)
+      return (
+         <>
+            <HeroBanner 
+               overlayHeading="albums" 
+               featureImg="/assets/imgs/all-sorts-of-questions.jpg"/>
 
-         <h3 className="ml_2">Albums</h3>
+            <h3 className="ml_2">Albums</h3>
 
-         <section className="feature_tiles">
-            <ul>
-               {data?.data?.albums_list?.length ? 
-                  data?.data?.albums_list?.map((album: Album) => (                 
-                     <AlbumTeaser
-                        key={album.id}  
-                        album={album} />
-                  ))
-                  : (<p><i>No albums</i></p>)}
-            </ul>
-         </section>
-      </>
-   )
-}
+            <section className="feature_tiles">
+               <ul>
+                  {data?.data?.albums_list?.length ? 
+                     data?.data?.albums_list?.map((album: Album) => (                 
+                        <AlbumTeaser
+                           key={album.id}  
+                           album={album} />
+                     ))
+                     : (<p><i>No albums</i></p>)}
+               </ul>
+            </section>
+         </>
+      )
+   }
    
 }
