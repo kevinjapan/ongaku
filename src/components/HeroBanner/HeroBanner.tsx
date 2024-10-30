0d0854f63d7e9e669684ca6cbe5053f06030ad9c
kevinjapan <61291412+kevinjapan@users.noreply.github.com>
2024-10-30 12:19:23 +0000

    rename 'type_slide_in..'

--- src/components/HeroBanner/HeroBanner.tsx
+++ src/components/HeroBanner/HeroBanner.tsx
@@ -20,7 +20,7 @@ export default function HeroBanner(props: HeroBannerProps) {
       if(local_prev_feature_img.current === props.featureImg) return
 
       const overlay_text = document.querySelector('.overlay')
-      overlay_text?.classList.remove('slide_in_from_left')
+      overlay_text?.classList.remove('type_in_from_left')
       setTimeout(() => setHeading(props.overlayHeading),1000)
       
       const cover_img = document.querySelector('.feature_img')   
@@ -36,7 +36,7 @@ export default function HeroBanner(props: HeroBannerProps) {
          
          // future: scrolling text integration still a bit fragile - how about a separate component for ScrollingHeading
          setTimeout(() => {
-            overlay_text?.classList.add('slide_in_from_left')
+            overlay_text?.classList.add('type_in_from_left')
             setHeading(props.overlayHeading)
          },600)
 
@@ -51,7 +51,7 @@ export default function HeroBanner(props: HeroBannerProps) {
       <section className="cover_block hero_block bg_navy dim_20 ">
          <img className="bg_img prev_feature_img z_n1" src={local_prev_feature_img.current} />
          <img className="bg_img feature_img pre_fade_in" src={props.featureImg} />
-         <div className="overlay slide_in_from_left_init">
+         <div className="overlay type_in_from_left_init">
             <h1>{heading}</h1>
             {props.overlayTagline ? <h4>{props.overlayTagline}</h4> : null}
          </div>
