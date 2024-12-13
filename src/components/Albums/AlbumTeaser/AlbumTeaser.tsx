78ebe2ee439f5bd386ee62b8265c6304b17ef5cf
kevinjapan <61291412+kevinjapan@users.noreply.github.com>
2024-12-13 12:18:22 +0000

    add link on title and img

--- src/components/Albums/AlbumTeaser/AlbumTeaser.tsx
+++ src/components/Albums/AlbumTeaser/AlbumTeaser.tsx
@@ -1,12 +1,20 @@
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+
+
 
 
 export default function AlbumTeaser(props: { album: Album }) {
 
+   const navigate = useNavigate()
+
+   const open_album = () => {
+      navigate(props.album.slug)
+   }
+
    return (
       <li className="flex flex_col gap_1">
-         <img src={props.album.feature_img} />
-         <h4>{props.album.title}</h4>
+         <img src={props.album.feature_img} onClick={open_album} className="cursor_pointer" />
+         <h4 onClick={open_album}>{props.album.title}</h4>
          <div className="flex px_2 space_around">
             <Link className="link" to={props.album.slug} >Tracks & Lyrics</Link>      
             {props.album.playlist && <a href={props.album.playlist} target="_blank">YouTube Playlist</a>}
