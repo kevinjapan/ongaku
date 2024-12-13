6265d1db4952da42fb0917e6ead299f8bd4e3242
kevinjapan <61291412+kevinjapan@users.noreply.github.com>
2024-12-13 13:46:57 +0000

    retain volume setting

--- src/components/AudioPlayer/AudioPlayerPlayer/AudioPlayerPlayer.tsx
+++ src/components/AudioPlayer/AudioPlayerPlayer/AudioPlayerPlayer.tsx
@@ -1,5 +1,9 @@
 import { useState, useEffect, useRef } from 'react'
 
+
+// to do : select play track from the card also
+// to do : play next track and go through list after current track finishes
+
 interface AudioPlayerPlayerProps {
    current_song: string
 }
@@ -9,23 +13,32 @@ export default function AudioPlayerPlayer({current_song} : AudioPlayerPlayerProp
 
    const [active_track, setActiveTrack] = useState('/audio/tell-me-how-you-see-me-clip.mp3')
 
+   const [active_volume, setActiveVolume] = useState(1)
+
    const audio_ref = useRef<HTMLAudioElement | null>(null)
 
    useEffect(() => {
       setActiveTrack(`/audio/${current_song}`)
-         setTimeout(() => {
-            if(audio_ref) {
-            audio_ref.current?.pause()
-            audio_ref.current?.load()
+      setTimeout(() => {
+         if(audio_ref) {
+         audio_ref.current?.pause()
+         audio_ref.current?.load()
+         if(audio_ref.current && audio_ref.current.volume) audio_ref.current.volume = active_volume
          if(current_song && current_song !== "select track to play") audio_ref.current?.play()
          }
       },600)
    },[current_song])
 
+   const change_volume = () => {
+      if(audio_ref !== null && audio_ref?.current?.volume) {
+         setActiveVolume(audio_ref?.current?.volume)
+      }
+   }
 
    return (
-      <div>
+      <div className="flex">
          <audio
+            onVolumeChange={change_volume}
             ref={audio_ref}
             controls
             key={active_track}>
