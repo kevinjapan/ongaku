1fad2f76e730397360ed760016e7f0c4c5cced79
kevinjapan <61291412+kevinjapan@users.noreply.github.com>
2024-12-18 16:03:30 +0000

    add content

--- src/views/HomeView.tsx
+++ src/views/HomeView.tsx
@@ -1,13 +1,21 @@
+import { useEffect } from 'react'
 import HeroBanner from '../components/HeroBanner/HeroBanner'
+import { init_fade_ins } from '../assets/js/behaviour'
 
 
 
 // HomeView
+// overlayTagline="where's your music taking you?"
 
 export default function HomeView() {
 
 
-//   overlayTagline="where's your music taking you?"
+   useEffect(() => {
+      setTimeout(() => {
+         init_fade_ins()
+      },500)
+   },[])
+
 
    return (
       <>
@@ -16,34 +24,92 @@ export default function HomeView() {
             featureImg="/imgs/soundscapes.jpg"
          />
 
-         <section>
-            <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet, quo inventore! 
-            </p>
-            <p>
-            Quia accusamus provident dolor unde aliquam adipisci, fugit officia eos reiciendis iure, ex in architecto 
-            assumenda corrupti quidem consequatur, veniam commodi ipsam doloribus earum corporis mollitia numquam autem minus. 
-            </p>
-            <p>            
-            Perferendis cum dicta ex nesciunt. Harum recusandae nulla natus consequatur corporis est, perferendis fugiat. 
-            Quos consequuntur quisquam fuga nam porro, minus quaerat vel nemo sint laudantium, nesciunt obcaecati ipsa? 
-            Voluptas neque architecto in iusto molestiae exercitationem nihil quas ipsam porro necessitatibus et sit quia 
-            magni libero, ratione deleniti assumenda eum consectetur distinctio officiis qui vitae? Rem nostrum adipisci 
-            quae eos hic eligendi, voluptatum necessitatibus, recusandae dolores modi exercitationem ducimus ipsum possimus 
-            maiores eaque fugiat blanditiis? Iure possimus dolorum eum placeat excepturi perferendis eveniet, numquam sint. 
-            </p>
-            <p>            
-            Deleniti nobis sequi similique illum voluptate, quibusdam voluptatibus! Unde eaque a nemo nisi sint vel illo totam 
-            fuga nulla? Sequi dolorum perspiciatis pariatur soluta, assumenda ducimus odit consequatur iusto, ea iure recusandae 
-            saepe! Repudiandae soluta eligendi quae assumenda magnam accusamus reprehenderit cumque. Explicabo beatae, blanditiis 
-            dicta maxime ea praesentium porro ratione error placeat cum quo ipsum enim doloribus, maiores vero in veritatis est 
-            voluptate aut libero neque, consequuntur consectetur ab architecto. Obcaecati minus cum non! Sint laboriosam reprehenderit 
-            repudiandae consectetur tenetur commodi excepturi necessitatibus corporis unde cupiditate reiciendis atque perferendis 
-            doloribus molestias autem eveniet, earum ea quia quis doloremque distinctio sit dignissimos accusamus minus. Illum hic 
-            </p>
-            <p>
-            Dignissimos quaerat, laborum fuga tempora dolorum veritatis voluptatum rem laboriosam eos blanditiis.
-            </p>            
+{/* to do : 
+   - gap appearing above timeline dimmer - remove yellow border on section 
+   - font color over timeline
+   - fix links on this page
+*/}
+         <section 
+            className="cover_block timeline dim_40 fade_in mt_0"
+            
+            style={{margin:0,border:'solid 3px yellow'}}
+            >
+
+            <img className="bg_img " src="./assets/imgs/edk-timeline-src.jpg" />
+
+            <div className="overlay">
+                  <h2 style={{fontWeight:300}}>iterations</h2>
+               <ul style={{fontSize:'1.2rem'}}>
+                  <li>
+                     lost for words<br/>
+                     <span style={{fontSize:'.9rem'}}>2017</span>
+                  </li>
+                  <li>
+                     rough not ready<br/>
+                     <span style={{fontSize:'.9rem'}}>2018</span>
+                  </li>
+                  <li>
+                     silver bullet<br/>
+                     <span style={{fontSize:'.9rem'}}>2018</span>
+                  </li>
+                  <li>
+                     life's a rhetorical question<br/>
+                     <span style={{fontSize:'.9rem'}}>2018</span>
+                  </li>
+                  <li>
+                     river<br/>
+                     <span style={{fontSize:'.9rem'}}>2019</span>
+                  </li>
+                  <li>
+                     the mountain that is<br/>
+                     <span style={{fontSize:'.9rem'}}>2019</span>
+                  </li>
+                  <li>
+                     beneath the waves<br/>
+                     <span style={{fontSize:'.9rem'}}>2020</span>
+                  </li>
+                  <li>
+                     psycosomatica<br/>
+                     <span style={{fontSize:'.9rem'}}>2021</span>
+                  </li>
+                  <li>
+                     clouds in the sky<br/>
+                     <span style={{fontSize:'.9rem'}}>2023</span>
+                  </li>
+               </ul>
+               <a href="https://www.youtube.com/@EvolutionDesuKa"
+               //  target=”_blank” 
+                  style={{marginTop:'2rem',marginLeft:'auto',marginRight:'auto'}}>
+                  <button>
+                     YouTube Channel
+                  </button>
+               </a>
+            </div>
+         </section>
+
+         {/* to do : add target="_blank" equivalent to following links */}
+         
+         <section className="feature_tiles bg_white fade_in">
+            <ul>
+               <li>
+                  <img src="/imgs/rhetorical-question-cover-sm.jpg" />
+                  <h4>life's a rhetorical question</h4>
+                  <button><a href="rhetorical-question.html">Tracks & Lyrics</a></button>
+                  <button><a href="https://www.youtube.com/watch?v=TnTlFNlFDME&list=PLKBwV2II2uYKHAhgqfQqmWhl2fT1fWnIa"> YouTube Playlist</a></button>
+               </li>
+               <li>
+                  <img src="./assets/imgs/river-cover-sm.jpg" />
+                  <h4>river</h4>
+                  <button><a href="river.html">Tracks & Lyrics</a></button>
+                  <button><a href="https://www.youtube.com/watch?v=hAwR-3cld54&list=PLKBwV2II2uYJgX3ufirbhVur5WX8H-c_u"> YouTube Playlist</a></button>
+               </li>
+               <li>
+                  <img src="./assets/imgs/beneath-the-waves-cover-sm.jpg" />
+                  <h4>beneath the waves</h4>
+                  <button><a href="beneath-the-waves.html">Tracks & Lyrics</a></button>
+                  <button><a href="https://www.youtube.com/watch?v=ME1ScAtrn28&list=PLKBwV2II2uYLVaidMrnYDB_rwPp7dyPX0"> YouTube Playlist</a></button>
+               </li>
+            </ul>
          </section>
       </>
    )
