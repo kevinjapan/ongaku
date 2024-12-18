6df3d753278062c828b56365abb107f02eb90d4a
kevinjapan <61291412+kevinjapan@users.noreply.github.com>
2024-12-18 16:03:12 +0000

    refactor AppNav dataset

--- src/utilities/appNav/appNav.ts
+++ src/utilities/appNav/appNav.ts
@@ -2,47 +2,62 @@
 export function get_nav_links () {
    return [
       {
+         id:1,
          label:'Albums',
          route:'albums',
          children:[
             {
-               id:0,
-               label:'all albums',
-               route:'albums'
+               id:100,
+               label:'albums',
+               route:'albums',
+               children: [
+                  {
+                     id:101,
+                     label:'beneath the waves',
+                     route:'albums/beneath-the-waves'
+                  },
+                  {
+                     id:102,
+                     label:'river',
+                     route:'albums/river'
+                  },
+                  {
+                     id:103,
+                     label:"life's a rhetorical question",
+                     route:'albums/lifes-a-rhetorical-question'
+                  }
+               ]
             },
             {
-               id:1,
-               label:'beneath the waves',
-               route:'albums/beneath-the-waves'
-            },
-            {
-               id:2,
-               label:'river',
-               route:'albums/river'
-            },
-            {
-               id:3,
-               label:"life's a rhetorical question",
-               route:'albums/lifes-a-rhetorical-question'
-            },
-            {
-               id:4,
-               label:'rough not ready',
-               route:'albums/rough-not-ready'
-            },
-            {
-               id:5,
-               label:'the wee song sketchbook',
-               route:'albums/the-wee-song-sketchbook'
+               id:200,
+               label:'collections',
+               route:'collections',
+               children: [
+                  {
+                     id:204,
+                     label:'rough not ready',
+                     route:'albums/rough-not-ready'
+                  },
+                  {
+                     id:205,
+                     label:'the wee song sketchbook',
+                     route:'albums/the-wee-song-sketchbook'
+                  }
+               ]
             }
          ]
       },
-      {label:'About',route:'about',
+      {
+         id:2,
+         label:'About',
+         route:'about',
          children:[
             {
-               id:1,
+               id:2001,
                label:'about',
                route:'/about'
-            },]},
+            }
+         ]
+      }
    ]
 }
