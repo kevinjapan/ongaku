d4df9d3c3fc59bc5fb63a0f59867375ea8ca76a4
kevinjapan <61291412+kevinjapan@users.noreply.github.com>
2024-12-20 12:42:11 +0000

    complete AppFooter

--- src/components/App/AppFooter/AppFooter.tsx
+++ src/components/App/AppFooter/AppFooter.tsx
@@ -1,44 +1,41 @@
+import { Link } from 'react-router-dom'
 import AppCopyright from '../AppCopyright/AppCopyright'
+
+
+// AppFooter
+// to do : actual links pls
+
 
 export default function AppFooter() {
 
-
    return (
       <section className="app_footer">
-         
          <ul>
-
             <li>
                <ul>
-                  <li>one</li>
-                  <li>two</li>
-                  <li>three</li>
-                  <li>four</li>
+                  <li>
+                     <Link className="link" to="/">Home</Link>
+                  </li>
+                  <li>
+                     <Link className="link" to="/albums">Albums</Link>
+                  </li>
+                  <li>
+                     <Link className="link" to="/about">About</Link>
+                  </li>
                </ul>
             </li>
             <li>
                <ul>
-                  <li>one</li>
-                  <li>two</li>
-                  <li>three</li>
-                  <li>four</li>
-                  <li>five</li>
-               </ul>
-               
+                  <li>
+                     <a href="https://www.youtube.com/@EvolutionDesuKa" target="_blank">YouTube</a>
+                  </li>
+               </ul>               
             </li>
             <li>
-               <ul>
-                  <li>one</li>
-                  <li>two</li>
-                  <li>three</li>
-               </ul>
-               
+               {/* empty links col */}
             </li>
-
          </ul>
-
          <AppCopyright/>
-
       </section>
    )
 }
