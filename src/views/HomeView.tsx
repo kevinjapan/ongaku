2f2218947ad1bbdfb742808fde9375e9ecbf9116
kevinjapan <61291412+kevinjapan@users.noreply.github.com>
2024-12-20 12:15:12 +0000

    enhance timeline UI

--- src/views/HomeView.tsx
+++ src/views/HomeView.tsx
@@ -1,5 +1,6 @@
 import { useEffect } from 'react'
 import HeroBanner from '../components/HeroBanner/HeroBanner'
+import AlbumTeaser from '../components/Albums/AlbumTeaser/AlbumTeaser'
 import { init_fade_ins } from '../assets/js/behaviour'
 
 
@@ -9,106 +10,116 @@ import { init_fade_ins } from '../assets/js/behaviour'
 
 export default function HomeView() {
 
-
    useEffect(() => {
+      window.scroll(0,0)
       setTimeout(() => {
          init_fade_ins()
       },500)
    },[])
 
+   // future : source dynamically (note: 'albums' folder specified here)
+   const featured_albums_list = [
+      {
+         "id":1,
+         "title":"beneath the waves",
+         "slug":"albums/beneath-the-waves",
+         "created_at":"6 Oct 2024",
+         "feature_img":"/imgs/beneath-the-waves-cover.jpg",
+         "playlist":"https://www.youtube.com/watch?v=ME1ScAtrn28&list=PLKBwV2II2uYLVaidMrnYDB_rwPp7dyPX0",
+         "tracks":[]
+      },
+      {
+         "id":2,
+         "title":"river",
+         "slug":"albums/river",
+         "created_at":"7 Oct 2024",
+         "feature_img":"/imgs/river-cover.jpg",
+         "playlist":"https://www.youtube.com/watch?v=hAwR-3cld54&list=PLKBwV2II2uYJgX3ufirbhVur5WX8H-c_u",
+         "tracks":[]
+      },
+      {
+         "id":3,
+         "title":"lifes a rhetorical question",
+         "slug":"albums/lifes-a-rhetorical-question",
+         "created_at":"8 Oct 2024",
+         "feature_img":"/imgs/rhetorical-question-cover.jpg",
+         "playlist":"https://www.youtube.com/watch?v=TnTlFNlFDME&list=PLKBwV2II2uYKHAhgqfQqmWhl2fT1fWnIa",
+         "tracks":[]
+      }
+   ]
 
    return (
       <>
          <HeroBanner 
             overlayHeading="evolution desu ka" 
-            featureImg="/imgs/soundscapes.jpg"
-         />
+            featureImg="/imgs/soundscapes.jpg"/>
 
-{/* to do : 
-   - gap appearing above timeline dimmer - remove yellow border on section 
-   - font color over timeline
-   - fix links on this page
-*/}
-         <section 
-            className="cover_block timeline dim_40 fade_in mt_0"
-            
-            style={{margin:0,border:'solid 3px yellow'}}
-            >
+         <section className="cover_block timeline dim_40 fade_in mt_0 no_user_select">
 
             <img className="bg_img " src="./assets/imgs/edk-timeline-src.jpg" />
 
             <div className="overlay">
-                  <h2 style={{fontWeight:300}}>iterations</h2>
-               <ul style={{fontSize:'1.2rem'}}>
-                  <li>
-                     lost for words<br/>
-                     <span style={{fontSize:'.9rem'}}>2017</span>
+
+               <h2 className="italic font_100">iterations</h2>
+
+               <ul className="font_1.2 text_center">
+                  <li className="text_center">
+                     Lost For Words<br/>
+                     <span className="font_.9 font_300">2017</span>
                   </li>
                   <li>
-                     rough not ready<br/>
-                     <span style={{fontSize:'.9rem'}}>2018</span>
+                     Rough Not Ready<br/>
+                     <span className="font_.9 font_300">2018</span>
                   </li>
                   <li>
-                     silver bullet<br/>
-                     <span style={{fontSize:'.9rem'}}>2018</span>
+                     Silver Bullet<br/>
+                     <span className="font_.9 font_300">2018</span>
                   </li>
                   <li>
-                     life's a rhetorical question<br/>
-                     <span style={{fontSize:'.9rem'}}>2018</span>
+                     Life's A Rhetorical Question<br/>
+                     <span className="font_.9 font_300">2018</span>
                   </li>
                   <li>
-                     river<br/>
-                     <span style={{fontSize:'.9rem'}}>2019</span>
+                     River<br/>
+                     <span className="font_.9 font_300">2019</span>
                   </li>
                   <li>
-                     the mountain that is<br/>
-                     <span style={{fontSize:'.9rem'}}>2019</span>
+                     The Mountain That Is<br/>
+                     <span className="font_.9 font_300">2019</span>
                   </li>
                   <li>
-                     beneath the waves<br/>
-                     <span style={{fontSize:'.9rem'}}>2020</span>
+                     Beneath The Waves<br/>
+                     <span className="font_.9 font_300">2020</span>
                   </li>
                   <li>
-                     psycosomatica<br/>
-                     <span style={{fontSize:'.9rem'}}>2021</span>
+                     Psycosomatica<br/>
+                     <span className="font_.9 font_300">2021</span>
                   </li>
                   <li>
-                     clouds in the sky<br/>
-                     <span style={{fontSize:'.9rem'}}>2023</span>
+                     Clouds In The Sky<br/>
+                     <span className="font_.9 font_300">2023</span>
                   </li>
                </ul>
-               <a href="https://www.youtube.com/@EvolutionDesuKa"
-               //  target=”_blank” 
-                  style={{marginTop:'2rem',marginLeft:'auto',marginRight:'auto'}}>
-                  <button>
-                     YouTube Channel
-                  </button>
-               </a>
+               <button className="light_btn_hover">
+                  <a href="https://www.youtube.com/@EvolutionDesuKa"
+                     target="_blank"
+                     style={{marginTop:'2rem',marginLeft:'auto',marginRight:'auto'}}>                  
+                     YouTube Channel                  
+                  </a>
+               </button>
             </div>
          </section>
-
-         {/* to do : add target="_blank" equivalent to following links */}
          
          <section className="feature_tiles bg_white fade_in">
-            <ul>
-               <li>
-                  <img src="/imgs/rhetorical-question-cover-sm.jpg" />
-                  <h4>life's a rhetorical question</h4>
-                  <button><a href="rhetorical-question.html">Tracks & Lyrics</a></button>
-                  <button><a href="https://www.youtube.com/watch?v=TnTlFNlFDME&list=PLKBwV2II2uYKHAhgqfQqmWhl2fT1fWnIa"> YouTube Playlist</a></button>
-               </li>
-               <li>
-                  <img src="./assets/imgs/river-cover-sm.jpg" />
-                  <h4>river</h4>
-                  <button><a href="river.html">Tracks & Lyrics</a></button>
-                  <button><a href="https://www.youtube.com/watch?v=hAwR-3cld54&list=PLKBwV2II2uYJgX3ufirbhVur5WX8H-c_u"> YouTube Playlist</a></button>
-               </li>
-               <li>
-                  <img src="./assets/imgs/beneath-the-waves-cover-sm.jpg" />
-                  <h4>beneath the waves</h4>
-                  <button><a href="beneath-the-waves.html">Tracks & Lyrics</a></button>
-                  <button><a href="https://www.youtube.com/watch?v=ME1ScAtrn28&list=PLKBwV2II2uYLVaidMrnYDB_rwPp7dyPX0"> YouTube Playlist</a></button>
-               </li>
+            <ul>              
+               {featured_albums_list?.length ?
+                  featured_albums_list?.map((album: Album) => (                 
+                     <AlbumTeaser
+                        key={album.id}  
+                        album={album} />
+                  ))
+                  : null
+               }
             </ul>
          </section>
       </>
