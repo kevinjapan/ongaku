d6a678b2d4a1f63f7947204b5ac2190dd6a75230
kevinjapan <61291412+kevinjapan@users.noreply.github.com>
2025-03-20 13:19:12 +0000

    simplify snapshot bg images

--- src/components/Snapshots/SnapShots.tsx
+++ src/components/Snapshots/SnapShots.tsx
@@ -1,11 +1,10 @@
 
-
 // SnapShots
 // displays snapshot images on background
+// we could prevent possible duplicates? - but rare and any solution likely messes w/ react's re-rendering - so good enough 
 
 
 export default function SnapShots() {
-
 
    const snapshot_imgs: Array<string> = [
       '/assets/imgs/snapshots/beached_boat.jpg',
@@ -19,24 +18,23 @@ export default function SnapShots() {
       '/assets/imgs/snapshots/stormy_shingle_beach.jpg'
    ]
 
-   const random_elem = (arr: string[]) => arr.length ? arr[Math.floor(Math.random() * arr.length)] : undefined
+   const random_elem = (arr: string[]) => {
+      const index = Math.floor(Math.random() * arr.length)
+      return arr.length ? arr[index] : undefined
+   }
 
    return (
-         <section className="snapshots_layer fade_in" style={{height:'fit-content',padding: '2rem',overflow:'hidden'}}>
-            <img
-               src={random_elem(snapshot_imgs)}
-               className="snapshot"
-               style={{top:'3%',left:'-8%',transform:'rotate(-5deg)',height:'50%'}}
-            />
-            <img
-               src={random_elem(snapshot_imgs)}
-               className="snapshot"
-               style={{top:'47%',left:'-8%',transform:'rotate(7deg)',height:'50%'}}
-            />
-
-            
-         </section>
-   )
-   
-      
+      <section className="snapshots_layer fade_in" style={{height:'fit-content',padding: '2rem',overflow:'hidden'}}>
+         <img
+            src={random_elem(snapshot_imgs)}
+            className="snapshot"
+            style={{top:'3%',left:'-8%',transform:'rotate(-5deg)',height:'50%'}}
+         />
+         <img
+            src={random_elem(snapshot_imgs)}
+            className="snapshot"
+            style={{top:'47%',left:'-8%',transform:'rotate(7deg)',height:'50%'}}
+         />
+      </section>
+   )  
 }
