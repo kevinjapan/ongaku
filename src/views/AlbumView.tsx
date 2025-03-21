b0715d070e53f96d83e492a9cbb84a06a480c879
kevinjapan <61291412+kevinjapan@users.noreply.github.com>
2025-03-21 13:00:58 +0000

    Lyrics and Links cta

--- src/views/AlbumView.tsx
+++ src/views/AlbumView.tsx
@@ -6,6 +6,8 @@ import Album from '../components/Albums/Album/Album'
 // AlbumView
 // layout wrapper around Album to prevent HeroBanner re-rendering/mounting every time we load a new album
 
+// to do : bug : eg enter url : http://localhost:3000/albums/anything -  should report 'not found' but throws error in TypeInTitle
+
 export default function AlbumView() {
 
    const [title, setTitle] = useState('')
@@ -21,6 +23,15 @@ export default function AlbumView() {
             overlayHeading={title}
             featureImg={feature_img}
          />
+
+         <button className="lyrics_and_links">
+            <a href="#lyrics_and_links">Lyrics & Links are below the fold</a>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" className="bi bi-arrow-bar-down" viewBox="0 0 16 16">
+               <path fillRule="evenodd" d="M1 3.5a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 0 1h-13a.5.5 0 0 1-.5-.5M8 6a.5.5 0 0 1 .5.5v5.793l2.146-2.147a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-3-3a.5.5 0 0 1 .708-.708L7.5 12.293V6.5A.5.5 0 0 1 8 6"/>
+            </svg>
+         </button> 
+
+         <div id="lyrics_and_links" className="fade_in"></div>
 
          <Album 
             set_title={setTitle}
