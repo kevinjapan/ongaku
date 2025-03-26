c0b2e63adaf8fbe4c8d06933b6529c4a15c44ed6
kevinjapan <61291412+kevinjapan@users.noreply.github.com>
2025-03-26 22:44:00 +0000

    expanded interface Track & TrackListItem

--- src/context/AppContext.tsx
+++ src/context/AppContext.tsx
@@ -20,7 +20,7 @@ export const AppContextProvider = ({children}: { children: React.ReactNode }) =>
    const prompt = 'select track to play'
 
    // the active_track playing
-   const [active_track, setActiveTrack] = useState({title:prompt,slug:'',audio:''} as TracksListItem)
+   const [active_track, setActiveTrack] = useState({title:prompt,slug:'',audio:'',released:'',album:''} as TracksListItem)
 
    // path for audio files
    const [audio_path] = useState('/audio/')
@@ -37,7 +37,7 @@ export const AppContextProvider = ({children}: { children: React.ReactNode }) =>
       // we play thru list to end
       let next_track_index = tracks_list.findIndex((track) => track.title === active_track.title) + 1
       next_track_index = next_track_index >= tracks_list.length ? next_track_index = 0 : next_track_index
-      next_track_index === 0 ? setActiveTrack({title:prompt,slug:'',audio:''}) : setActiveTrack(tracks_list[next_track_index])
+      next_track_index === 0 ? setActiveTrack({title:prompt,slug:'',audio:'',released:'',album:''}) : setActiveTrack(tracks_list[next_track_index])
    
    }
 
@@ -46,77 +46,107 @@ export const AppContextProvider = ({children}: { children: React.ReactNode }) =>
          {
             "title":"12 Bars",
             "slug":"12-bars-clip.mp3",
-            "audio":"12-bars-clip.mp3"
+            "audio":"12-bars-clip.mp3",
+            "released":"2018",
+            "album":"Rough Not Ready"
          },
          {
             "title":"Find My Feet",
             "slug":"find-my-feet-clip.mp3",
-            "audio":"find-my-feet-clip.mp3"
+            "audio":"find-my-feet-clip.mp3",
+            "released":"2020",
+            "album":"Beneath The Waves"
          },
          {
             "title":"Foetal",
             "slug":"foetal-clip.mp3",
-            "audio":"foetal-clip.mp3"
+            "audio":"foetal-clip.mp3",
+            "released":"2018",
+            "album":"Life's a Rhetorical Question"
          },
          {
             "title":"I Don't Want To Be Here Anymore",
             "slug":"i-dont-want-to-be-here-anymore-clip.mp3",
-            "audio":"i-dont-want-to-be-here-anymore-clip.mp3"
+            "audio":"i-dont-want-to-be-here-anymore-clip.mp3",
+            "released":"2019",
+            "album":"The Wee Song Sketchbook"
          },
          {
             "title":"Inside The Music",
             "slug":"inside-the-music-clip.mp3",
-            "audio":"inside-the-music-clip.mp3"
+            "audio":"inside-the-music-clip.mp3",
+            "released":"2018",
+            "album":"Life's a Rhetorical Question"
          },
          {
             "title":"Nothing's Secret",
             "slug":"nothings-secret-clip.mp3",
-            "audio":"nothings-secret-clip.mp3"
+            "audio":"nothings-secret-clip.mp3",
+            "released":"2018",
+            "album":"Life's a Rhetorical Question"
          },
          {
             "title":"Release Your Love",
             "slug":"release-your-love-clip.mp3",
-            "audio":"release-your-love-clip.mp3"
+            "audio":"release-your-love-clip.mp3",
+            "released":"2021",
+            "album":"Rough Not Ready"
          },
          {
             "title":"River",
             "slug":"river-clip.mp3",
-            "audio":"river-clip.mp3"
+            "audio":"river-clip.mp3",
+            "released":"2019",
+            "album":"River"
          },
          {
             "title":"Sand",
             "slug":"sand-clip.mp3",
-            "audio":"sand-clip.mp3"
+            "audio":"sand-clip.mp3",
+            "released":"2019",
+            "album":"River"
          },
          {
             "title":"SoundScapes",
             "slug":"soundscapes-clip.mp3",
-            "audio":"soundscapes-clip.mp3"
+            "audio":"soundscapes-clip.mp3",
+            "released":"2018",
+            "album":"Rough Not Ready"
          },
          {
             "title":"Tell Me How You See Me",
             "slug":"tell-me-how-you-see-me-clip.mp3",
-            "audio":"tell-me-how-you-see-me-clip.mp3"
+            "audio":"tell-me-how-you-see-me-clip.mp3",
+            "released":"2019",
+            "album":"River"
          },
          {
             "title":"Tether",
             "slug":"tether-clip.mp3",
-            "audio":"tether-clip.mp3"
+            "audio":"tether-clip.mp3",
+            "released":"2018",
+            "album":"The Wee Song Sketchbook"
          },
          {
             "title":"The Crashing Waves",
             "slug":"the-crashing-waves-clip.mp3",
-            "audio":"the-crashing-waves-clip.mp3"
+            "audio":"the-crashing-waves-clip.mp3",
+            "released":"2020",
+            "album":"Beneath The Waves"
          },
          {
             "title":"The Lighthouse Song",
             "slug":"the-lighthouse-song-clip.mp3",
-            "audio":"the-lighthouse-song-clip.mp3"
+            "audio":"the-lighthouse-song-clip.mp3",
+            "released":"2020",
+            "album":"Beneath The Waves"
          },
          {    
             "title":"This Picture",
             "slug":"this-picture-clip.mp3",
-            "audio":"this-picture-clip.mp3"
+            "audio":"this-picture-clip.mp3",
+            "released":"2021",
+            "album":"The Wee Song Sketchbook"
          }
       ])
    },[])
